1d65a6f99d6e28a2296caebec61ec42eda39bc0c
kamiya <78120565+jcf04567@users.noreply.github.com>
2023-04-15 20:24:08 +0900

    アクセスを制御する、まで終了

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../service/firebaseConfig";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Main = () => {
   const [user, setUser] = useState("");
@@ -17,13 +17,18 @@ const Main = () => {
   const _signOut = async () => {
     await signOut(auth);
     navigate("/SignIn/");
-  }
+  };
   return (
     <>
-      <h1>Main page</h1>
-      {user ? <h2>{user.email}</h2> : navigate("/SignIn/") }
-      {/* <h2>{user?.email}</h2> */}
-      <button onClick={ _signOut }>SignOut</button>
+      {!user ? (
+        <Navigate to={`/SignIn/`} />
+      ) : (
+        <>
+          <h1>Main page</h1>
+          <h2>{user?.email}</h2>
+          <button onClick={_signOut}>SignOut</button>
+        </>
+      )}
     </>
   );
 };
